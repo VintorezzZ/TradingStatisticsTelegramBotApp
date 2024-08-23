e0595876b35b4dab993e4a06d8fc9a13827f745c
VintorezzZ <sergeevmisha16@gmail.com>
2024-08-23 20:20:57 +0300

    Ready to work project

---
+++ TradingStatisticsTelegramBotApp/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace TradingStatisticsTelegramBotApp;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
---
+++ TradingStatisticsTelegramBotApp/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using TradingStatisticsTelegramBotCore;
+
+namespace TradingStatisticsTelegramBotApp;
+
+public partial class MainPage : ContentPage
+{
+    int count = 0;
+    private CancellationTokenSource _cts;
+    private BotProcessor _botProcessor;
+
+    public MainPage()
+    {
+        InitializeComponent();
+        
+        _cts = new CancellationTokenSource();
+        _botProcessor = new BotProcessor();
+    }
+
+    private async void OnCounterClicked(object sender, EventArgs e)
+    {
+        await _botProcessor.Start(_cts);
+        
+        count++;
+
+        if (count == 1)
+            CounterBtn.Text = $"Clicked {count} time";
+        else
+            CounterBtn.Text = $"Clicked {count} times";
+
+        SemanticScreenReader.Announce(CounterBtn.Text);
+    }
+}
